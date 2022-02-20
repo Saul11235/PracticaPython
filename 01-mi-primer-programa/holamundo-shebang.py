d55bd8da0bf2ebb451de49bb3f56f20462abe5e6
Edwin Saul <edwinsaulpm@gmail.com>
2022-02-19 23:00:32 -0500

    gitignore holamundo

---
+++ 01-mi-primer-programa/holamundo-shebang.py
@@ -0,0 +1,9 @@
+#!usr/bin/python
+
+#la primera linea no es obligatoria pero sirve para indicar la ruta especifica del interprete se llama SHEBANG
+#y esta configurado para un entorno Linux-Unix
+
+#esto es un comentario no se ejecuta
+
+print("hola mundo") 
+input() #detiene el prog  
--- 01-mi-primer-programa/holamundo.py
+++ 01-mi-primer-programa/holamundo.py
@@ -1,2 +1,3 @@
 #esto es un comentario no se ejecuta
 print("hola mundo")
+input() #detiene el prog
